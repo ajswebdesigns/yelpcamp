735f1201c3d9af017bff116a5b6db4b2fd66e30f
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-09 12:21:28 -0400

    section 414 completed

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
-const campground = require('./models/campground');
-
+const Campground = require('./models/campground');
 
 // Connect to mongoose and create a DB
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -21,10 +20,16 @@ app.set('view engine', 'ejs')
 // Set the path
 app.set('views', path.join(__dirname, 'views'));
 
-
+// Index Route
 app.get('/campgrounds', async (req, res)=>{
- const campgrounds = await campground.find({})
+ const campgrounds = await Campground.find({})
  res.render('campgrounds/index', {campgrounds})
+})
+
+// Show Route:
+app.get('/campgrounds/:id', async (req, res)=>{
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', {campground})
 })
 
 app.listen(3000, ()=>{
